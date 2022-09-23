fa453e1256e6ff6ab12ccd5a98cddd861dfd5bba
josue <josue@LAPTOP-I3VRBKPN>
2022-09-23 09:31:06 -0600

    Agregar archivos de proyecto.

---
+++ Division de Constructor/Division.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_de_Constructor
+{
+    internal class Division
+    {
+        double v1, v2;
+
+        public Division()
+        {
+        }
+
+        public Division(double v1, double v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public double Dividdir()
+        {
+            return v1 / v2;
+        }
+    }
+}
